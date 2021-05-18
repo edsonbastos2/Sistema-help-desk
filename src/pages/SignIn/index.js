68decd34027de12b7917a7e4f5f8b6a83fe04ac4
Edson-working <edsonbastos2@hotmail.com>
2021-05-18 15:10:18 -0300

    Criado página de cadastro

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,32 @@
+import { useState } from 'react';
+import {Link} from 'react-router-dom';
+import './signIn.css';
+
 
 export default function SignIn(){
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+
+    function handleSubmit(){
+        alert('Clicou')
+    }
+
     return(
-        <h1>Página de login</h1>
+        <div className="container-area">
+            <div className="login">
+                <div className="logo-area">
+                    <img src="" alt="Logo sistema"/>
+                </div>
+
+                <form onSubmit={handleSubmit}>
+                    <h1>Entrar</h1>
+                    <input type="text" placeholder="exemplo@padrao.com" value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input type="password" placeholder="**********" value={password} onChange={e => setPassword(e.target.value)}/>
+                    <button type="submit">Acessar</button>
+                </form>
+                <Link to="/register">Criar uma conta</Link>
+            </div>
+        </div>
     )
 }
