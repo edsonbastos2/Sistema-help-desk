dfe2fa21ff7d4fa6c728fbb3a6e230cbf3501281
Edson-working <edsonbastos2@hotmail.com>
2021-05-18 14:13:15 -0300

    Feito configuração de rotas

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,6 @@
+
+export default function SignIn(){
+    return(
+        <h1>Página de login</h1>
+    )
+}
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+
+let firebaseConfig = {
+    apiKey: "AIzaSyDyLOuTdrvGpyQyujhCMsqpkBkIce6lT6o",
+    authDomain: "project-singular-oficial.firebaseapp.com",
+    projectId: "project-singular-oficial",
+    storageBucket: "project-singular-oficial.appspot.com",
+    messagingSenderId: "555219097450",
+    appId: "1:555219097450:web:204e9ad5451c468fb4f713",
+    measurementId: "G-L0Q6SD0HVD"
+  };
+
+  if(!firebase.apps.length){
+      firebase.initializeApp(firebaseConfig);
+  }
