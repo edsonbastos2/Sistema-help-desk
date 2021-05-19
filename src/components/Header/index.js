0ce55bce4c3247408ad97d1d5a321b7d5880cb43
Edson Bastos <edsonbastos2@hotmail.com>
2021-05-19 00:09:48 -0300

    Criado aside/header e instalado react-icons

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import {useContext} from 'react';
+import {AuthContext} from '../../contexts/auth';
+import {Link} from 'react-router-dom';
+import avatar from '../../assets/avatar.png';
+import {FiHome, FiUser, FiSettings,FiPackage} from 'react-icons/fi';
+import './header.css';
+
+export default function Header(){
+
+    const {user} = useContext(AuthContext);
+
+    return(
+        <div className="sidebar">
+            <div>
+                <img src={user.avatarUrl === null ? avatar : user.avatarUrl} alt="Foto avatar"/>
+            </div>
+            <Link to="/dashboard">
+                <FiHome color="#FFF" size={24}/>
+                Chamados
+            </Link>
+            <Link to="/dashboard">
+                <FiUser color="#FFF" size={24}/>
+                Clientes
+            </Link>
+            <Link to="/dashboard">
+                <FiSettings color="#FFF" size={24}/>
+                Configurações
+            </Link>
+            <Link to="/dashboard">
+                <FiPackage color="#FFF" size={24}/>
+                Estoque
+            </Link>
+        </div>
+    )
+}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,11 +1,13 @@
 import {useContext} from 'react';
-import {AuthContext} from '../../contexts/auth'
+import {AuthContext} from '../../contexts/auth';
+import Header from '../../components/Header';
 
 export default function Dashboard(){
 
     const {signOut} = useContext(AuthContext)
     return(
         <>
+        <Header/>
         <h1>DashBoard</h1>
         <button onClick={() => signOut()}>Fazer Logout</button>
         </>
