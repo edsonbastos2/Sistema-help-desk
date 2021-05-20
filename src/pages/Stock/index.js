af4816e9a34ef8f7c47109c46a27a95ca63ba735
Edson-working <edsonbastos2@hotmail.com>
2021-05-20 18:30:55 -0300

    Consumindo dados na tabela de devices, criado order by e função handleMore

--- src/pages/Stock/index.js
+++ src/pages/Stock/index.js
@@ -4,9 +4,95 @@ import {FiTrendingUp} from 'react-icons/fi';
 import {Link} from 'react-router-dom';
 import {FiSearch, FiEdit2} from 'react-icons/fi';
 import './stock.css';
+import { useEffect, useState } from 'react';
+import firebase from '../../services/firebaseConnection';
 
+const listRef = firebase.firestore().collection('device').orderBy('created', 'desc');
 
 export default function Stock(){
+
+    const [devices, setDevices] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [loadMore, setLoadMore] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
+    const [lastDocs, setLastDocs] = useState()
+
+    useEffect(() =>{
+        loadDevices()
+    }, [])
+
+    async function loadDevices(){
+        await listRef.limit(5)
+        .get()
+        .then((snapshot) =>{
+            updateState(snapshot)
+        })
+        .catch((error)=>{
+            console.log('Erro ao carregar ' + error)
+            setLoadMore(false)
+        })
+
+        setLoading(false)
+    }
+
+    async function updateState(snapshot){
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if(!isCollectionEmpty){
+            let lista = []
+            snapshot.forEach(doc => {
+                lista.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    manufacturer: doc.data().manufacturer,
+                    device_type: doc.data().device_type,
+                    created: doc.data().created,
+                    location: doc.data().location,
+                    model: doc.data().model,
+                    owner: doc.data().owner,
+                    serial_number: doc.data().serial_number,
+                    status: doc.data().status
+                })
+            });
+
+            const lastDoc = snapshot.docs[snapshot.docs.length - 1]; //Pega o utimo documento buscado 
+
+            setDevices( devices => [...devices, ...lista])
+            setLastDocs(lastDoc)
+        }else{
+            setIsEmpty(true)
+        }
+
+        setLoadMore(false)
+    }
+
+
+
+   async function handleMore(){
+        await listRef.startAfter(lastDocs).limit(5)
+        .get()
+        .then((snapshot => {
+            updateState(snapshot)
+        }))
+    }
+
+    if(loading){
+        return(
+            <div>
+                <Header/>
+                <div className="content stock">
+                    <Title title="Tabela de Estoque">
+                        <FiTrendingUp size={25}/>
+                    </Title>
+
+                    <div className="container">
+                        <h5>Buscando Equipamentos...</h5>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return(
         <div>
             <Header/>
@@ -23,45 +109,51 @@ export default function Stock(){
                     <table>
                         <thead>
                             <tr>
-                                <th scope="col">Nome do Dispositivo</th>
-                                <th scope="col">Fabricante do Dispositivo</th>
-                                <th scope="col">Modelo do Dispositivo</th>
-                                <th scope="col">Número de serie</th>
-                                <th scope="col">Tipo de Dispositivo</th>
-                                <th scope="col">Setor alocado</th>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Fabricante</th>
+                                <th scope="col">Modelo</th>
+                                <th scope="col">Lote</th>
+                                <th scope="col">Tipo</th>
+                                <th scope="col">Setor</th>
                                 <th scope="col">Usuário</th>
                                 <th scope="col">Data de entrada</th>
-                                <th scope="col">Data de Saída</th>
                                 <th scope="col">Status</th>
                                 <th scope="col">Ações</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td data-label="Nome">ti002</td>
-                                <td data-label="Fabricante">Lenovo</td>
-                                <td data-label="Modelo">L100</td>
-                                <td data-label="Serie">2001010200</td>
-                                <td data-label="Tipo">Notebook</td>
-                                <td data-label="Setor">T.I</td>
-                                <td data-label="Usuário">Edson Bastos</td>
-                                <td data-label="Data">23/01/2019</td>
-                                <td data-label="Saida">23/01/2019</td>
-                                <td data-label="Status">
-                                    <span className="badge" style={{backgroundColor:'#5cb85c'}}>Ativo</span>
-                                </td>
-                                <td data-label="Ações">
-                                    <button className="action" style={{backgroundColor: '#3583f6'}}>
-                                        <FiSearch color="#fff" size={17}/>
-                                    </button>
-                                    <button className="action" style={{backgroundColor: '#F6a935'}}>
-                                        <FiEdit2 color="#fff" size={17}/>
-                                    </button>
-                                </td>
-                            </tr>
+                            {devices.map((item, index) => {
+                                return(
+                                    <tr key={index}>
+                                        <td data-label="Nome">{item.name}</td>
+                                        <td data-label="Fabricante">{item.manufacturer}</td>
+                                        <td data-label="Modelo">{item.model}</td>
+                                        <td data-label="Serie">{item.serial_number}</td>
+                                        <td data-label="Tipo">{item.device_type}</td>
+                                        <td data-label="Setor">{item.location}</td>
+                                        <td data-label="Usuário">{item.owner}</td>
+                                        <td data-label="Data">{item.created}</td>
+                                        <td data-label="Status">
+                                            <span className="badge" 
+                                            style={{backgroundColor: item.status ? '#5cb85c' : '#ddd'}}>{item.status}</span>
+                                        </td>
+                                        <td data-label="Ações">
+                                            <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                                <FiSearch color="#fff" size={17}/>
+                                            </button>
+                                            <button className="action" style={{backgroundColor: '#F6a935'}}>
+                                                <FiEdit2 color="#fff" size={17}/>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                            
                         </tbody>
                     </table>
-
+                    
+                    {loadMore && <h3>Carregando dados...</h3>}
+                    { !loadMore && !isEmpty && <button className="btn btn-success mt-4" onClick={handleMore}>Carregar mais</button>}
                 </>
 
             </div>
